fbe405c2dbd9ab06888ae8675bee9ea0b623ed54
Karim Samaha <kareem.mahmoud1237@gmail.com>
2024-08-30 13:51:53 +0300

    Android-work

---
+++ constants/Locales.js
@@ -0,0 +1,235 @@
+export const HomeLocales = {
+    ar: {
+        categoires: "التصنيفات"
+    }
+}
+
+export const AccountLocales = {
+    ar: {
+        accountDetails: "بيانات الحساب",
+        name: "الأسم",
+        email: "البريد الالكتروني",
+        address: "العنوان",
+        phone: "الهاتف",
+        save: "حفظ"
+    }
+}
+
+export const CartLocales = {
+    ar: {
+        noProducts: "لا يوجد منتجات في عربة التسوق",
+        shopping: "التسوق",
+        delete: "مسح",
+        productAdds: "اضافات الورود",
+        couponActivated: "تم تفيعل كوبون خصم",
+        couponTitle: "هل لديك كوبون خصم؟",
+        couponPlaceHolder: "كوبون خصم",
+        wrongCoupon: "الكود خاطئ",
+        couponMinumum: "الحد الادني لاستخدام الكوبون",
+        totalNoVat: "المجموع غير شامل الضريبة",
+        discount: "قسيمة التخفيض",
+        totalToApplyVat: "المجموع الخاضع للضريبة",
+        vatText: "ضريبة القيمة المضافة",
+        shippingCost: "تكاليف الشحن",
+        totalPlusVat: "مجموع المنتجات شامل ضريبة القيمة المضافة",
+        total: "مجموع الكلي",
+        shippingHeader: "اختيار شركة الشحن",
+        completePayment: "اتمام الدفع",
+        loginForPayment: "تسجيل الدخول لاتمام الدفع"
+    }
+}
+
+export const CustomerServiceLocales = {
+    ar: {
+        title: "خدمة العملاء والشكاوى والاقتراحات",
+        content: "نحن نرحب دائماً بتعاونك معنا ومشاركتنا كل مقترحاتك. يمكنك الاتصال على ارقامنا او عبر المحادثه المباشره من خلال WhatsApp"
+    }
+}
+
+export const FavLocales = {
+    ar: {
+        empty: "لم يتم اضافة منتجات في القائمة"
+    }
+}
+
+export const LicenceLocal = {
+    ar: {
+        title: "الشهادات المتعلقه بالمتجر",
+        content: "رقم السجل التجاري 1010603970",
+        subContent: "رقم الشهاده الضريبيه 310457496200003"
+    }
+}
+export const RefundLocal = {
+    ar: {
+        title: "سياسة الاستبدال والاسترجاع",
+        content: "سياسة الإستبدال والإسترجاع: يحق للعميل الاستبدال قبل البدء بتجهيز المنتج ، لكن بعد خروج الطلب للتوصيل لا يمكن الاستبدال أو الاسترجاع وفقاً لطبيعة المنتج و الخدمة المقدمة. في حال وجود خلل أو تلف بعد التوصيل فيحق للعميل انطلاقاً من مبدأ الثقة المتبادلة إستبدال المنتج او الحصول على كامل القيمه المدفوعه. آلية ارجاع المبالغ: يتم ارجاع المبلغ بنفس طريقة الدفع التي تمت ."
+    }
+}
+export const TermsLocal = {
+    ar: {
+        title: "الشروط والأحكام",
+        content: "باستخدام موقع وتطبيق الزهرة العصرية فأنت توافق على الالتزام بهذه الشروط والأحكام، وفي حال عدم موافقتك على الشروط والأحكام يتعين عليك عدم استخدام الموقع والتطبيق ، وعليه فإننا نحتفظ بحقنا بتغيير الشروط والأحكام من وقت لآخر وبالتالي يتعين عليك الاطلاع على هذه الشروط والأحكام بشكلٍ دوري ، ويعتبر استمرار استخدامك لموقع وتطبيق الزهرة العصرية بمثابة موافقة على الشروط والأحكام المطبقة في وقت استخدامك للموقع والتطبيق كجزء من التزامنا بجعل موقع وتطبيق الزهرة العصرية مكانا ترغب في زيارته باستمرار فإننا نرحب بكافة تعليقاتك على أي من السياسات أو القواعد وشكراً جزيلاً."
+    }
+}
+export const LoginLocal = {
+    ar: {
+        otpSentText: "تم ارسال رمز التحقق الي",
+        login: "تسجيل الدخول",
+        email: "البريد الالكتروني",
+        otpButton: "ارسال رمز التحقق",
+        codeHasBeenSent: "تم ارسال رمز التحقق الي",
+        phone: "رقم الجوال",
+        loginViaEmail: "تسجيل الدخول عبر البريد الالكتروني",
+        loginViaPhone: "تسجيل الدخول عبر الهاتف",
+        wrongInput: "البيانات غير صحيحه",
+        somthingWentWrong: "حدث حطأ ما",
+        otpPlaceHolder: "رمز التحقق",
+        name: "الاسم"
+
+    }
+}
+export const OrderLocal = {
+    ar: {
+        successOrder: "تم استلام طلبك بنجاح",
+        header: "الطلبات السابقة",
+        showMore: "عرض المزيد"
+    }
+}
+
+export const PaymentLocal = {
+    ar: {
+        methodSelect: "اختر وسيلة الدفع",
+        bankHeader: "بينات الحساب المصرفي",
+        bankName: "اسم المصرف",
+        accountName: "اسم المالك",
+        accountNum: "رقم الحساب",
+        iban: "الأيبان",
+        swift: "السويفت",
+        note1: 'بالضغط على اختيار "ادفع الاّن" فأنت توافق على',
+        note2: "شروط الإستخدام",
+        payNow: "ادفع الآن"
+    }
+}
+export const ProductLocal = {
+    ar: {
+        discount: "خصم",
+        priceBefore: "سعر قبل الخصم",
+        price: "السعر",
+        aboutProduct: "عن المنتج",
+        addToCart: "اضف الي السلة",
+        butAsGift: "شراء كهديه",
+        ourRviews: "تقيمات عملائنا",
+        noReviews: "لا يوجد تقييمات حتى اللحظة",
+        
+    }
+}
+
+export const TrendyBenfLocal = {
+    ar: {
+        header1: "شحن مجاني داخل الرياض",
+        header2: "لست بحاجة لمعرفة العنوان (فريقنا سيفعل ذلك نيابة عنك)",
+        header3: "أجود أنواع الزهور",
+        header4: "اشتري الان و اربح نقاط ترندي"
+    }
+}
+export const ProfileLocals = {
+    ar: {
+        orders: "طلباتي",
+        profile: "حسابي",
+        favList: "قائمتي",
+        logOut: "تسجيل الخروج"
+    }
+}
+export const searchLocals = {
+    ar: {
+        result: "نتائج البحث",
+        noResult: "لا يوجد نتائج"
+    }
+}
+
+export const subCategorisLocal = {
+    ar: {
+        subCtg: "التصنيفات الفرعية",
+        noProducts: "لا يوجد منتجات حاليا",
+        more: "عرض المزيد"
+    }
+}
+
+export const ProductForm = {
+    ar: {
+        senderName: "اسم المرسل",
+        reciverName: "اسم المرسل اليه",
+        date: "تاريخ التوصيل (متاح من 2 الظهر الي 11م)",
+        dateError: "يجد تحديد تاريخ التوصيل",
+        time: "وقت التوصيل ( من 2 الظهر الي 11م)",
+        timeError: "يجد تحديد وقت التوصيل",
+        unvalidTimeError: "يجد تحديد وقت التوصيل بشكل صحيح",
+        cardText: "نص البطاقة - اكتب إهدائك هنا ( + 6.00 ر.س )",
+        noAddressSelect: "لا اريد نحديد العنوان (فريق الدعم سيتواصل مع المستلم)",
+        address: "العنوان",
+        addressError: "يجد تحديد العنوان",
+        productAdds: "اضافات الورود",
+        phone: "رقم المستلم",
+        phoneError: "يجب تحديد رقم المستلم",
+        video: "ارفاق رابط (فيديو او صوت)",
+        cardsSelect: "اختيار كروت الاهداء",
+        cheoSelect: "اختيار شيكولاتة",
+        ballonsSelect: "اختيار بالونات",
+        sticksSelect: "اختيار تغريسات",
+        giftCardSelect: "اختيار قسائم شرائية",
+        addToCart: "اضف الي السلة",
+        giftAdd: "شراء كهديه",
+        delete: "مسح",
+        edit: "تعديل",
+        giftRequiredError: "لم يتم اختيار اضافات",
+    }
+}
+
+export const OrderItemLocal = {
+    ar: {
+        refNum: "الرقم المرجعي",
+        rate: "تقيم",
+        date: "التاريخ",
+        price: "السعر",
+        qty: "الكمية",
+        shipping: "طريقة الشحن",
+        reciverName: "بيانات المستلم",
+        address: "عنوان الشحن",
+        cardText: "نص بطاقة",
+        adds: "اضافات الورود",
+
+    }
+}
+
+export const ReviewLocal = {
+    ar: {
+        success: "تم ارسال تقيمك",
+        reviewProductHeader: "رأيك عن المنتج",
+        reviewProduct: "قيم المنتج",
+        storeReviewHeader: "رأيك عن الموقع",
+        reviewStore: "قيم الموقع",
+        submit: "إرسال التقييم"
+    }
+}
+
+export const StaticLinksLocale = {
+    ar: {
+        terms: "الشروط والأحكام",
+        refund: "سياسة الاستبدال والاسترجاع",
+        customerService: "خدمة العملاء والشكاوى والاقتراحات",
+        licences: "التراخيص"
+    }
+}
+
+export const storeDeleviryLocale = {
+    ar: {
+        name: "الاسم",
+        nameError: "يجب ادخال الاسم",
+        phone: "رقم الهاتف",
+        phoneError: "يجب ادخال رقم الهاتف",
+        date: "تاريخ الاستلام (متاح من 2 الظهر إلى 11 مساءً)",
+        dateError: "يجد تحديد تاريخ التوصيل",
+        confirm: "تأكيد البيانات",
+        edit: "تعديل البيانات"
+    }
+}
