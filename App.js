56ce0fbd5f4490ceca1e77a47518390ac5df5b82
Karim Samaha <kareem.mahmoud1237@gmail.com>
2024-09-23 16:03:43 +0300

    ...

--- App.js
+++ App.js
@@ -3,13 +3,39 @@ import { StyleSheet, Text, View, I18nManager } from "react-native";
 import { ModalPortal } from "react-native-modals";
 import { Provider } from "react-redux";
 import StackNavigator from "./navigation/StackNavigator";
-import {store, persistor} from "./store";
+import { store, persistor } from "./store";
 import { UserContext } from "./UserContext";
 import { PersistGate } from 'redux-persist/integration/react';
+import { useEffect, useState } from "react";
 
+I18nManager.allowRTL(false)
 
-I18nManager.forceRTL(false);
 export default function App() {
+  const [layoutChanged, setLayoutChanged] = useState(false);  // Track if layout changed
+
+  useEffect(() => {
+    const forceLTR = true;  // Set to true for LTR, false for RTL
+
+    // Check if the current direction matches the desired one
+    if (forceLTR && I18nManager.isRTL) {
+      I18nManager.forceRTL(false); 
+      I18nManager.allowRTL(false)
+      setLayoutChanged(true)
+
+       // Force LTR if system is RTL
+      // setLayoutChanged(true);  // Trigger re-render to apply new layout
+    } else if (!forceLTR && !I18nManager.isRTL) {
+      I18nManager.forceRTL(true);  // Force RTL if system is LTR
+      setLayoutChanged(true);  // Trigger re-render to apply new layout
+    }
+  }, []);
+
+
+  if (layoutChanged) {
+    console.log({I18nManager})
+    return null;  // Render null while layout direction is being set
+  }
+  
   return (
     <>
       <Provider store={store}>
